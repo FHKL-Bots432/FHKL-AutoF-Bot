2c56bbdc3d09cc88adbbe49399b1de6cc11725e8
FHKL-Bots432 <96911602+FHKL-Bots432@users.noreply.github.com>
2022-02-04 22:44:15 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -26,5 +26,5 @@ class Translation(object):
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://t.me/+SuKq6KMnVa4yZTJl">Click Me</a></i>
 """
